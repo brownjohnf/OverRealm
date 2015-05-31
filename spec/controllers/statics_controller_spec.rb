bd8b8281800162f94b526915bfb026bf1c60a76a
John (Jack) Brown <jackb@tune.com>
2015-05-31 11:10:37 -0700

    integrate guard; add first tests

--- spec/controllers/statics_controller_spec.rb
+++ spec/controllers/statics_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe StaticsController, type: :controller do
+  describe 'GET index' do
+    it 'succeeds' do
+      get :index
 
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the index template' do
+      get :index
+
+      expect(response).to render_template('index')
+    end
+  end
 end
