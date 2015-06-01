0ec87ad8bc9befd10c863e3dd4b16887adf7bdff
John (Jack) Brown <jackb@tune.com>
2015-05-31 22:12:04 -0700

    add support for active admin user control

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register AdminUser do
+ActiveAdmin.register User do
   permit_params :email, :password, :password_confirmation
 
   index do
@@ -8,7 +8,6 @@ ActiveAdmin.register AdminUser do
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
-    column :type
     actions
   end
 
@@ -18,7 +17,7 @@ ActiveAdmin.register AdminUser do
   filter :created_at
 
   form do |f|
-    f.inputs "Admin Details" do
+    f.inputs "User Details" do
       f.input :email
       f.input :password
       f.input :password_confirmation
